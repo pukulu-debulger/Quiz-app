6610b4df6d0eadafad44d7e2d9b9bd34fad0b678
Pre-pukulu <bed-com-50-21@unima.ac.mw>
2025-12-28 18:26:00 +0200

    logic to loop through answers and keep truck of collect answers and give final score

--- src/componets/Results.jsx
+++ src/componets/Results.jsx
@@ -1,10 +1,22 @@
 
 
 function Results({userAnswers, questions}) {
+    function getScore() {
+        let finalScore = 0;
+        
+        userAnswers.forEach((answer, index) => {
+            if (answer === questions[index].correctAnswer) {
+                finalScore ++;
+            }
+        });
+        return finalScore;
+    }
+    const score = getScore();
+
     return(
         <div className="results-container">
             <h2>Quiz Results</h2>
-            <p> Your Score: </p>
+            <p> Your Score: {score}/{questions.length} </p>
 
             <button className="restart-button">Retake Quiz</button>
         </div>
